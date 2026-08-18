5040a3eb2f3ab3831d1b50c82730b720e489c0aa
sapthagiri-98 <sapthagiri.98@gmail.com>
2026-08-18 23:01:18 +0200

    Update config.js

--- Portal/config.js
+++ Portal/config.js
@@ -23,7 +23,7 @@ window.PORTAL_CONFIG = {
   SUPABASE_FNS: ["validateLogin","getUsers","smBootstrap","smSearchStudents",
     "smGetStudent","smCheckStudentId","smAdmitStudent","smUpdateStudent",
     "smUpdateEnrollment","smDeleteStudent","smPromoteStudents","smPromoteClass",
-    "smLeaveStudent","smRejoinStudent","smBulkChangeClass","smAddCustomField",
+    "smLeaveStudent","smRejoinStudent","smBulkChangeClass","smAddCustomField", "smGetRollNumbers",
     "smExportStudents","smImportStudents","smChangeStudentId"],
   SUPABASE_ATT_BASE: "https://denmdonkbmcmqfxlunpy.supabase.co/functions/v1/attendance-api",
   SUPABASE_ATT_FNS: ["getClasses","getHolidaysForDate","getClassAttendanceSummary",
